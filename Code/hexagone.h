84d09a46817dd49f2b995dc6d0997eef0f624a01
valentin <valentin.ridoux@gmail.com>
2025-10-31 17:30:03 +0100

    Finalisation des class hexagone et tuile avec implémentation des méthodes de base (constructeur, destructeur, getteur...)

--- Code/hexagone.h
+++ Code/hexagone.h
@@ -2,11 +2,22 @@
 #include <cstring>
 
 
-typedef enum { Habitation, Marche, Caserne, Temple, Jardin } Type;
+typedef enum { Habitation, Marche, Caserne, Temple, Jardin, Place } Type;
 
 
 class Hexagone {
 private:
+	int niveau, etoiles;
 	Type type;
 	bool recouvert;
+
+	Hexagone(Type t, int e = 0, int n = 1, bool r = false) : type(t), etoiles(e), niveau(n), recouvert(r) {};
+	~Hexagone() {}
+	Hexagone(const Hexagone& h) = delete;
+	Hexagone& operator=(const Hexagone&) = delete;
+	
+
+	Type getType() const { return type; }
+	int getEtoiles() const { return etoiles; }
+	int getNiveau() const { return niveau; }
 };
--- Code/tuiles.h
+++ Code/tuiles.h
@@ -1,11 +1,22 @@
 #include <iostream>
 #include <cstring>
+#include "Hexagone.h"
 
 typedef enum{1 2 3} Orientation;
 
-class Tuile {
+class Tuile : public Hexagone {
 private :
-	int id, niveau;
+	int id;
 	Orientation orientation;
-	hexagone hexagones[3];
+	Hexagone hexagones[3];
+
+	Tuile(int i, const Hexagone& h1, const Hexagone& h2, const Hexagone& h3) : id(i), hexagones({ h1, h2, h3 }), orientation(1) {}
+	~Tuile(){}
+	Tuile(const Tuile& t) = delete;
+	Tuile& operator=(const Tuile& t) = delete;
+
+	int getId() const { return id; }
+	const Hexagone& getHexagone(int i) const { return hexagones[i]; }
+	Orientation getOrientation() const { return Orientation; }
+
 };
