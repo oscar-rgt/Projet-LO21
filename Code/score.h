af1fa1d597fc72caac9b41c8ccf03f8c180119a2
nmo490 <166531716+nmo490@users.noreply.github.com>
2025-11-06 13:22:07 +0100

    Update score.h

--- Code/score.h
+++ Code/score.h
@@ -1,1 +1,17 @@
+#ifndef SCORE_H
+#define SCORE_H
+
+#include "hexagone.h"
+
+// Structure d'une tuile posée
+typedef struct {
+    Type type;   // couleur/type
+    int niveau;  // hauteur
+    int etoiles; // multiplicateur
+} ScoreInput;
+
+// voisins[6] contiendra les types des 6 hexagones autour (0 si vide)
+int calcul_points(ScoreInput centre, Type voisins[6]);
+
+#endif
 
