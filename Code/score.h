a65a4e41f38d302ca3ed1bf6ce19610506a3a2d2
nmo490 <166531716+nmo490@users.noreply.github.com>
2025-11-07 10:40:30 +0100

    Update score.h

--- Code/score.h
+++ Code/score.h
@@ -1,12 +1,33 @@
 #ifndef SCORE_H
 #define SCORE_H
 
+#include <map>
+#include "joueur.h"
+#include "cite.h"
 #include "hexagone.h"
-#include <vector>
 
-using namespace std;
+enum class TypeQuartier {
+    Habitation,
+    Marche,
+    Caserne,
+    Temple,
+    Jardin
+};
 
-int calculPoints(Hexagone& h, vector<vector<Hexagone>>& plateau, int ligne, int colonne);
-int calculScore(vector<vector<Hexagone>>& plateau);
+class Score {
+private:
+    Joueur* joueur;             // Joueur concerné
+    Cité* cite;                 // Référence à la cité du joueur
+    std::map<TypeQuartier, int> pointsParType;  // Score détaillé par type de quartier
+    int total;                  // Score total final
+
+public:
+    Score(Joueur* j, Cité* c);
+    void calculerScore();
+    int calculerScoreType(TypeQuartier type);// Calcul du score d’un seul type de quartier
+    int getTotal() const;
+    int getScoreType(TypeQuartier type) const;
+    const Joueur* getJoueur() const;
+};
 
 #endif
