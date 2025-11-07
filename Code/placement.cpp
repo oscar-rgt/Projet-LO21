305c548a21467f4d706b32749388edef0a2878d8
Jeannnou <jdeboudt@etu.utc.fr>
2025-11-07 18:29:03 +0100

    Enhance tile placement logic in Plateau class
    Added checks for tile placement conditions including support and adjacency.

--- Code/placement.cpp
+++ Code/placement.cpp
@@ -7,4 +7,59 @@ void Plateau::placer(Tuile* t, Coord c){
 	carte[c] = t->getHexagone(0);
 	carte[c.sudEst()] = t->getHexagone(1);
 	carte[c.sudOuest()] = t->getHexagone(2);
+	
+	if(c.z==1){
+        std::vector<Coord> voisins = {
+                // Voisins de c (qui ne sont pas sudEst ou sudOuest)
+                {c.x + 1, c.y, c.z}, {c.x - 1, c.y, c.z},
+                {c.x, c.y - 1, c.z},
+                {c.x, c.y, c.z + 1}, {c.x, c.y, c.z - 1},
+                // Voisins de sudEst (qui ne sont pas c ou sudOuest)
+                {c.x - 1, c.y + 1, c.z},
+                {c.x, c.y + 2, c.z},
+                {c.x, c.y + 1, c.z + 1}, {c.x, c.y + 1, c.z - 1},
+                // Voisins de sudOuest (qui ne sont pas c ou sudEst)
+                {c.x + 2, c.y + 1, c.z},
+                {c.x + 1, c.y + 2, c.z},
+                {c.x + 1, c.y + 1, c.z + 1}, {c.x + 1, c.y + 1, c.z - 1}
+            };
+            
+            for (const auto& voisin : voisins) {
+                if (!estLibre(voisin)) {
+                    touchePlateau = true;
+                    break;
+                }
+            }
+            
+            if (!touchePlateau ) { 
+                printf("Placement impossible")
+                return; 
+            }
+    }
+    else {
+    // Vérifier que chaque hexagone de la tuile est soutenu par un hexagone en dessous
+    Coord dessous_c = {c.x, c.y, c.z - 1};
+    Coord dessous_sudEst = {c.x, c.y + 1, c.z - 1};
+    Coord dessous_sudOuest = {c.x + 1, c.y + 1, c.z - 1};
+    
+    if (estLibre(dessous_c) || estLibre(dessous_sudEst) || estLibre(dessous_sudOuest)) {
+        printf("Placement impossible ");
+        return;
+    }
+    
+    // Vérifier qu'elle est à cheval sur au moins 2 tuiles différentes
+    Tuile* tuile_dessous_c = carte[dessous_c].getTuile();
+    Tuile* tuile_dessous_sudEst = carte[dessous_sudEst].getTuile();
+    Tuile* tuile_dessous_sudOuest = carte[dessous_sudOuest].getTuile();
+    
+    bool deuxTuilesDifferentes = (tuile_dessous_c != tuile_dessous_sudEst) || 
+                                  (tuile_dessous_c != tuile_dessous_sudOuest) || 
+                                  (tuile_dessous_sudEst != tuile_dessous_sudOuest);
+    
+    if (!deuxTuilesDifferentes) {
+        printf("Placement impossible ");
+        return;
+    }
+}
+	
 }
