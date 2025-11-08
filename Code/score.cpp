c20015b3090be33aaabb01d544b102f3e7aabc27
nmo490 <166531716+nmo490@users.noreply.github.com>
2025-11-08 17:03:15 +0100

    Update score.cpp

--- Code/score.cpp
+++ Code/score.cpp
@@ -1,6 +1,5 @@
 #include "score.h"
 #include <iostream>
-
 using namespace std;
 
 Score::Score(Joueur* j, Cité* c)
@@ -29,16 +28,13 @@ const Joueur* Score::getJoueur() const {
 //score global
 void Score::calculerScore() {
     total = 0;
-
     for (auto type : {TypeQuartier::Habitation, TypeQuartier::Marche,
                       TypeQuartier::Caserne, TypeQuartier::Temple,
-                      TypeQuartier::Jardin}) {
-
+                      TypeQuartier::Jardin, TypeQuartier::Place}) {
         int pts = calculerScoreType(type);
         pointsParType[type] = pts;
         total += pts;
     }
-
     joueur->setPoints(total);
 }
 
@@ -46,39 +42,40 @@ void Score::calculerScore() {
 int Score::calculerScoreType(TypeQuartier type) {
     int score = 0;
     int multiplicateur = 0;
-
-    // récupère tous les hexagones de la cité
     const vector<Hexagone*>& hexas = cite->getHexagones();
 
-    // calcul du multiplicateur avec les étoiles
+    //calculer les étoiles
     for (auto* h : hexas) {
-        if (h->getType() == Type::Place && h->getTypeAssocie() == type) {
-            multiplicateur += h->getEtoiles();
+        if (h->getType() == Type::Place) {
+            switch (type) {
+                case TypeQuartier::Habitation: multiplicateur += h->getEtoiles(); break;
+                case TypeQuartier::Marche: multiplicateur += h->getEtoiles(); break;
+                case TypeQuartier::Caserne: multiplicateur += h->getEtoiles(); break;
+                case TypeQuartier::Temple: multiplicateur += h->getEtoiles(); break;
+                case TypeQuartier::Jardin: multiplicateur += h->getEtoiles(); break;
+                case TypeQuartier::Place: multiplicateur += h->getEtoiles(); break;
+            }
         }
     }
 
     if (multiplicateur == 0) return 0;
 
-    // scoring cas par cas
+    //cas par cas selon tuile
     for (auto* h : hexas) {
         switch (type) {
             case TypeQuartier::Habitation:
-                if (h->getType() == Type::Habitation) {
-                    score += 1;
-                }
+                if (h->getType() == Type::Habitation) score += 1;
                 break;
-
             case TypeQuartier::Marche:
                 if (h->getType() == Type::Marche) {
-                    int pointsAdj = 0;
+                    int ptsAdj = 0;
                     for (auto* v : cite->getAdjacents(h)) {
                         if (v->getType() != Type::Marche && v->getType() != Type::Place)
-                            pointsAdj++;
+                            ptsAdj++;
                     }
-                    score += pointsAdj;
+                    score += ptsAdj;
                 }
                 break;
-
             case TypeQuartier::Caserne:
                 if (h->getType() == Type::Caserne) {
                     bool isIsolee = true;
@@ -91,20 +88,19 @@ int Score::calculerScoreType(TypeQuartier type) {
                     if (isIsolee) score += 1;
                 }
                 break;
-
             case TypeQuartier::Temple:
                 if (h->getType() == Type::Temple) {
-                    if (cite->getAdjacents(h).size() == 6)
-                        score += 2;
+                    if (cite->getAdjacents(h).size() == 6) score += 2;
                 }
                 break;
-
             case TypeQuartier::Jardin:
-                if (h->getType() == Type::Jardin)
-                    score += 1;
+                if (h->getType() == Type::Jardin) score += 1;
+                break;
+            case TypeQuartier::Place:
                 break;
         }
     }
+
     score *= multiplicateur;
     return score;
 }
