d34221bbfe89b8153123ba401970d69e49fd5c56
nmo490 <166531716+nmo490@users.noreply.github.com>
2025-11-08 16:47:21 +0100

    Update joueur.h

--- Code/joueur.h
+++ Code/joueur.h
@@ -3,15 +3,17 @@
 
 #include <string>
 
+using namespace std;
+
 class Joueur {
 private:
-    std::string nom;
+    string nom;
     int points;
     int pierres;
 
 public:
-    Joueur(const std::string& n);
-    const std::string& getNom() const;
+    Joueur(const string& n);
+    const string& getNom() const;
     int getPoints() const;
     int getPierres() const;
     void setPoints(int p);
