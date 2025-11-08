e289f48ac8ff0c68af11f764ce8d600939bf03fc
vridoux <rdx.valentin@gmail.com>
2025-11-08 17:20:57 +0100

    Methode affiche de tuiles enlevée

--- Code/tuiles.h
+++ Code/tuiles.h
@@ -21,7 +21,6 @@ public:
 	Orientation getOrientation() const { return orientation; }
 	Hexagone& getHexagone(int i) { return hexagones[i]; }
 	unsigned int getPrix() const { return prix; }
-	void affiche();
 	Tuile(unsigned int i, Hexagone& h1, Hexagone& h2, Hexagone& h3, unsigned int p) : id(i), orientation(N), hexagones{ h1, h2, h3 }, prix(p) {}
 	~Tuile() {}
 };
