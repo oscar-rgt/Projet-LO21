84b4921b732fe7d1f1ae7b30a182707bfcf7c4c1
Jeannnou <jdeboudt@etu.utc.fr>
2025-12-01 14:58:55 +0100

    Add save.cpp for saving game state

---
+++ Code/save.cpp
@@ -0,0 +1,13 @@
+#include <ftream>
+#include "partie.h"
+#include <string>
+/*
+bool Save::savePartie(const string& nameFich) {
+  string nomFich= "partie"+ to_string(Partie::getID())+".txt";
+  ofstream fichier(nomFich);
+  if (!fichier.is_open()) {
+        cout << " Erreur : impossible de créer le fichier de sauvegarde" << endl;
+        return false;
+    }
+    
+    */
